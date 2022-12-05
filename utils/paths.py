3810e931eb05b1c735263715e35d0b729f1e90fd
Yuri <yurimaas@hotmail.com>
2022-12-05 08:09:04 +0100

    2022 Day 5

--- utils/paths.py
+++ utils/paths.py
@@ -1,5 +1,5 @@
 PATHS = {
-    2020: "C:\\Users\\Yuri\\Documents\\Programming\\Advent of Code\\years\\AoC2020\\data",
-    2021: "C:\\Users\\Yuri\\Documents\\Programming\\Advent of Code\\years\\AoC2021\\data",
-    2022: "C:\\Users\\Yuri\\Documents\\Programming\\Advent of Code\\years\\AoC2022\\data",
+    2020: "C:\\Users\\yurim\\Documents\\Programming\\Advent of Code\\years\\AoC2020\\data",
+    2021: "C:\\Users\\yurim\\Documents\\Programming\\Advent of Code\\years\\AoC2021\\data",
+    2022: "C:\\Users\\yurim\\Documents\\Programming\\Advent of Code\\years\\AoC2022\\data",
 }
--- years/AoC2022/task_5.py
+++ years/AoC2022/task_5.py
@@ -7,15 +7,45 @@ class Task5(Task):
     YEAR = 2022
     TASK_NUM = 5
 
-    @debug
-    @timer(YEAR, TASK_NUM)
-    def part_1(self, data: list) -> int:
-        return len(data)
+    def preprocess(self, data: list) -> tuple:
+        index = data.index('')
+        stacks = {int(i): [] for i in data[index-1] if i != ''}
+        for line in data[:index-1]:
+            crates = line.split('|')
+            for i, crate in enumerate(crates):
+                if crate == '':
+                    continue
+                crate = crate[1:2]
+                stacks[i+1].append(crate)
+        moves = []
+        for move in data[index+1:]:
+            moves.append([int(move[1]), int(move[3]), int(move[5])])
+        return stacks, moves
 
     @debug
     @timer(YEAR, TASK_NUM)
-    def part_2(self, data: list) -> int:
-        return len(data)
+    def part_1(self, data: list) -> str:
+        stacks, moves = data
+        for move in moves:
+            count, source, target = move
+            for _ in range(count):
+                crate = stacks[source].pop(0)
+                stacks[target].insert(0, crate)
+        return "".join([stacks[column][0] for column in stacks])
+
+    @debug
+    @timer(YEAR, TASK_NUM)
+    def part_2(self, data: list) -> str:
+        stacks, moves = data
+        for move in moves:
+            count, source, target = move
+            crane_hold = []
+            for _ in range(count):
+                crate = stacks[source].pop(0)
+                crane_hold.insert(0, crate)
+            for crate in crane_hold:
+                stacks[target].insert(0, crate)
+        return "".join([stacks[column][0] for column in stacks])
 
 
 if __name__ == "__main__":
