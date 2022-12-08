742c8e208cb561c00d409b001e80ed4b4517299b
Yuri <yurimaas@hotmail.com>
2022-12-08 22:22:19 +0100

    2022 Day 8

--- years/AoC2022/task_8.py
+++ years/AoC2022/task_8.py
@@ -7,15 +7,52 @@ class Task8(Task):
     YEAR = 2022
     TASK_NUM = 8
 
+    def preprocess(self, data: list) -> list:
+        return [list(map(int, row)) for row in data]
+
     @debug
     @timer(YEAR, TASK_NUM)
     def part_1(self, data: list) -> int:
-        return len(data)
+        visible_trees = 0
+        for i in range(len(data)):
+            for j in range(len(data[0])):
+                height = data[i][j]
+                # Check horizontal rows
+                if (all(val < height for val in data[i][:j])
+                        or all(val < height for val in data[i][j+1:])):
+                    visible_trees += 1
+                    continue
+                # Check vertical columns
+                if (all(data[val][j] < height for val in range(i))
+                        or all(data[val][j] < height for val in range(i+1, len(data)))):
+                    visible_trees += 1
+        return visible_trees
 
     @debug
     @timer(YEAR, TASK_NUM)
     def part_2(self, data: list) -> int:
-        return len(data)
+        max_score = 0
+        for i in range(len(data)):
+            for j in range(len(data[0])):
+                height = data[i][j]
+                score = 1
+                # Trees to right, left, up, down
+                score *= self.seen_trees(height, data[i][j+1:])
+                score *= self.seen_trees(height, data[i][:j][::-1])
+                score *= self.seen_trees(height, [data[val][j] for val in range(i)][::-1])
+                score *= self.seen_trees(height, [data[val][j] for val in range(i+1, len(data))])
+                if score > max_score:
+                    max_score = score
+        return max_score
+
+    @staticmethod
+    def seen_trees(height: int, row: list) -> int:
+        trees = 0
+        for tree in row:
+            trees += 1
+            if tree >= height:
+                break
+        return trees
 
 
 if __name__ == "__main__":
