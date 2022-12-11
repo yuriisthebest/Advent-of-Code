41b34f742e8ab54d2b18be1666a33973b2bc100f
Yuri <yurimaas@hotmail.com>
2022-12-11 10:02:47 +0100

    2022 Day 10

--- years/AoC2022/task_10.py
+++ years/AoC2022/task_10.py
@@ -10,12 +10,43 @@ class Task10(Task):
     @debug
     @timer(YEAR, TASK_NUM)
     def part_1(self, data: list) -> int:
-        return len(data)
+        register = 1
+        cycle = 0
+        signal_strengths = 0
+        for instruction in data:
+            iterator = 2 if instruction[0] == "addx" else 1
+            for _ in range(iterator):
+                cycle += 1
+                if cycle % 40 == 20:
+                    signal_strengths += cycle * register
+            if instruction[0] == "addx":
+                register += int(instruction[1])
+        return signal_strengths
 
     @debug
     @timer(YEAR, TASK_NUM)
     def part_2(self, data: list) -> int:
-        return len(data)
+        result = ""
+        register = 1
+        cycle = 0
+        signal_strengths = 0
+        for instruction in data:
+            iterator = 2 if instruction[0] == "addx" else 1
+            for _ in range(iterator):
+                cycle += 1
+                if abs(((cycle-1) % 40) - register) <= 1:
+                    result += "#"
+                else:
+                    result += " "
+                if cycle % 40 == 0:
+                    result += "\n"
+                # To compute part 1
+                if cycle % 40 == 20:
+                    signal_strengths += cycle * register
+            if instruction[0] == "addx":
+                register += int(instruction[1])
+        print(result)
+        return signal_strengths
 
 
 if __name__ == "__main__":
