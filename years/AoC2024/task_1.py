2267f2be0692e51d30dd7a213153c5121c3e1e4a
Yuri <yurimaas@hotmail.com>
2024-12-01 21:53:29 +0100

    Finished day 1 2024

--- years/AoC2024/task_1.py
+++ years/AoC2024/task_1.py
@@ -7,15 +7,35 @@ class Task1(Task):
     YEAR = 2024
     TASK_NUM = 1
 
-    @debug
-    @timer(YEAR, TASK_NUM)
-    def part_1(self, data: list) -> int:
-        return len(data)
+    def preprocess(self, data: list) -> list:
+        left_locations = []
+        right_locations = []
+        for line in data:
+            left_locations.append(int(line[0]))
+            right_locations.append(int(line[3]))
+        return [left_locations, right_locations]
 
     @debug
     @timer(YEAR, TASK_NUM)
-    def part_2(self, data: list) -> int:
-        return len(data)
+    def part_1(self, data: list[list, list]) -> int:
+        left = data[0]
+        left.sort()
+        right = data[1]
+        right.sort()
+        total = 0
+        for left_num, right_num in zip(left, right):
+            total += abs(left_num - right_num)
+        return total
+
+    @debug
+    @timer(YEAR, TASK_NUM)
+    def part_2(self, data: list[list, list]) -> int:
+        left = data[0]
+        right = data[1]
+        total = 0
+        for left_num in left:
+            total += left_num * right.count(left_num)
+        return total
 
 
 if __name__ == "__main__":
