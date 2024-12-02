37d8f38b1acd0ce122a55dbddaa327e84eefd217
Yuri <yurimaas@hotmail.com>
2024-12-02 18:46:23 +0100

    Finished day 2 2024

--- years/AoC2024/README.md
+++ years/AoC2024/README.md
@@ -1,5 +1,5 @@
 # Advent of Code 2024
-![Collected starts](https://img.shields.io/static/v1?style=for-the-badge&logo=apachespark&label=stars&message=0/50&color=success&logoColor=yellow)
+![Collected starts](https://img.shields.io/static/v1?style=for-the-badge&logo=apachespark&label=stars&message=2/50&color=success&logoColor=yellow)
 
 ## Tasks
 A list of the tasks of this year with explanations, difficulty judgements,
@@ -13,15 +13,15 @@ A list of the tasks of this year with explanations, difficulty judgements,
 - ![](https://via.placeholder.com/15/000000/000000?text=+) Very Hard
 
 
-### ![](https://via.placeholder.com/15/ffffff/000000?text=+) Day 1: 
-* **Question:** 
-* **Solution:** 
-* **Thoughts:** 
+### ![](https://via.placeholder.com/15/ffffff/000000?text=+) Day 1: Historian Hysteria
+* **Question:** Use one list to search for counts of numbers in another list
+* **Solution:** Loop over list 1, count occurrences in list 2; naïve method
+* **Thoughts:** Okay start, not too difficult, not too too easy. Theming seems lower this year, will see in the coming weeks
 
-### ![](https://via.placeholder.com/15/ffffff/000000?text=+) Day 2: 
-* **Question:** 
-* **Solution:** 
-* **Thoughts:** 
+### ![](https://via.placeholder.com/15/ffffff/000000?text=+) Day 2: Red-Nosed Reports
+* **Question:** Check if a report is safe, ignoring one bad number in the sequence
+* **Solution:** Loop over the numbers until a bad level is encountered. Remove either the left or right level (one of which is bad) and check again if the report is safe (without removing any more levels).
+* **Thoughts:** More difficult than I expected at day 2 early in the morning, got stuck a little on trying to ignore numbers instead of removing and checking again.
 
 ### ![](https://via.placeholder.com/15/ffffff/000000?text=+) Day 3: 
 * **Question:** 
--- years/AoC2024/task_2.py
+++ years/AoC2024/task_2.py
@@ -7,15 +7,55 @@ class Task2(Task):
     YEAR = 2024
     TASK_NUM = 2
 
+    def preprocess(self, data: list) -> list:
+        return [[int(num) for num in line] for line in data]
+
+    def is_safe_dampened(self, report: list, ascending: bool, safeties: int = 1) -> bool:
+        i = 0
+        while i < len(report) - 1:
+            if i >= len(report) - 1:
+                continue
+            num = report[i]
+            next_num = report[i + 1]
+            if ascending and (num >= next_num or num < next_num - 3):
+                if safeties > 0:
+                    return self.recurse_without_bad_level(ascending, i, report, safeties)
+                else:
+                    return False
+            if not ascending and (num <= next_num or num > next_num + 3):
+                if safeties > 0:
+                    return self.recurse_without_bad_level(ascending, i, report, safeties)
+                else:
+                    return False
+            i += 1
+        return True
+
+    def recurse_without_bad_level(self, ascending, i, report, safeties):
+        safeties -= 1
+        new_1 = report.copy()
+        new_2 = report.copy()
+        del new_1[i]
+        del new_2[i + 1]
+        return (self.is_safe_dampened(new_1, ascending, safeties)
+                or self.is_safe_dampened(new_2, ascending, safeties))
+
     @debug
     @timer(YEAR, TASK_NUM)
     def part_1(self, data: list) -> int:
-        return len(data)
+        num_safe = 0
+        for report in data:
+            if self.is_safe_dampened(report, True, 0) or self.is_safe_dampened(report, False, 0):
+                num_safe += 1
+        return num_safe
 
     @debug
     @timer(YEAR, TASK_NUM)
     def part_2(self, data: list) -> int:
-        return len(data)
+        num_safe = 0
+        for report in data:
+            if self.is_safe_dampened(report, True, 1) or self.is_safe_dampened(report, False, 1):
+                num_safe += 1
+        return num_safe
 
 
 if __name__ == "__main__":
