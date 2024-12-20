2bd1a7616f44221c1803d43b734dc1d48dba617d
Yuri <yurimaas@hotmail.com>
2024-12-20 22:50:03 +0100

    Finished day 19 2024

--- years/AoC2024/task_19.py
+++ years/AoC2024/task_19.py
@@ -1,3 +1,4 @@
+from functools import cache
 from utils.decorators import timer, debug
 from utils.task import Task
 
@@ -7,15 +8,34 @@ class Task19(Task):
     YEAR = 2024
     TASK_NUM = 19
 
+    @cache
+    def count_designs(self, patterns: list, design: str) -> int:
+        if design == "":
+            return 1
+        num_designs = 0
+        for pat in patterns:
+            if design[:len(pat)] == pat:
+                num_designs += self.count_designs(patterns, design[len(pat):])
+        return num_designs
+
     @debug
     @timer(YEAR, TASK_NUM)
     def part_1(self, data: list) -> int:
-        return len(data)
+        num_solved = 0
+        patterns = tuple([pat[:-1] if pat[-1] == "," else pat for pat in data[0]])
+        for design in data[2:]:
+            if self.count_designs(patterns, design) > 0:
+                num_solved += 1
+        return num_solved
 
     @debug
     @timer(YEAR, TASK_NUM)
     def part_2(self, data: list) -> int:
-        return len(data)
+        num_solved = 0
+        patterns = tuple([pat[:-1] if pat[-1] == "," else pat for pat in data[0]])
+        for design in data[2:]:
+            num_solved += self.count_designs(patterns, design)
+        return num_solved
 
 
 if __name__ == "__main__":
